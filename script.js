b377ca34cde3d640541d01d59206a31c42e3439b
NiedsonDV <54153962+NietzDV@users.noreply.github.com>
2019-12-05 09:28:01 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-function testarformulario(form) {
+function testarformulario() {
+    let form = document.getElementById("texto").value;
+
+    console.log(form)
+    if(form == ""){
+        alert("Por favor, preencha o formulário corretamente");
+        return true;
+        
+    }else{
+        alert("Sugestão enviada com sucesso, aguarda pois leremos e responderemos se podermos!");
+        return false;
+    }
     
-    alert("Sugestão enviada com sucesso, aguarda pois leremos e responderemos se podermos!");
-	return true;
+	
 }
