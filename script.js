7d46578033d96f8b5460ee170159f223bb4eef92
NiedsonDV <54153962+NietzDV@users.noreply.github.com>
2019-12-05 08:24:49 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 function testarformulario(form) {
-    if(form.texto.value == ""){
-        alert("Insira a mensagem");
-		form.mensagem.focus();
-		return false;
-    }
+    
     alert("Sugestão enviada com sucesso, aguarda pois leremos e responderemos se podermos!");
 	return true;
 }
