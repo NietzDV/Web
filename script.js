ef6384f3ba6417ab3b8cc034168d876f0f6281d7
NiedsonDV <54153962+NietzDV@users.noreply.github.com>
2019-12-05 02:56:35 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-function validar() {
-    
-    var nome = formulario.nome.value;
- 
-      if (nome == "") {
-        alert('Preencha o campo com seu nome');
-          formulario.nome.focus();
-          return false;
-       }
-
-alert("Sugestão enviada com sucesso, aguarde rerposta, pois iremos reponder com todo o amor e carinho!S2");
-    return true;
+function testarformulario(form) {
+    if(form.texto.value == ""){
+        alert("Insira a mensagem");
+		form.mensagem.focus();
+		return false;
+    }
+    alert("Sugestão enviada com sucesso, aguarda pois leremos e responderemos se podermos!");
+	return true;
 }
