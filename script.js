244d864c57e6cc311cbdd0eb05b42df3fb1f2158
NiedsonDV <54153962+NietzDV@users.noreply.github.com>
2019-12-05 01:59:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function validar() {
+    
+    var nome = formulario.nome.value;
+ 
+      if (nome == "") {
+        alert('Preencha o campo com seu nome');
+          formulario.nome.focus();
+          return false;
+       }
+
+alert("Sugestão enviada com sucesso, aguarde rerposta, pois iremos reponder com todo o amor e carinho!S2");
+    return true;
+}
